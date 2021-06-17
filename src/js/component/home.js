bb24eaa78fca2aef328217ade5a5fd96eda0b577
apolopino <apolo.pino@gmail.com>
2021-06-17 00:55:33 +0000

    lista interfaz, starting hooks

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [count, setCount] = useState(0);
+	// function tick() {
+	// 	useState(count + 1);
+	// }
+	// setInterval(tick, 1000);
+	setInterval(useState, 1000);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="row d-flex justify-content-center">
+			<div className="box lg text-center">
+				<span className="far fa-clock"></span>
+			</div>
+			<div className="box text-center">0</div>
+			<div className="box text-center">0</div>
+			<div className="box text-center">0</div>
+			<div className="box text-center">0</div>
+			<div className="box text-center">0</div>
+			<div className="box text-center">{useState(count + 1)}</div>
 		</div>
 	);
 }
